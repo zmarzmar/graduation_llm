6c220019687e604b5f0f5eae384cd71f943dc93b
Dongmyoung Min <zmarzmzm@naver.com>
2025-07-07 18:26:48 +0900

    :tada:Start: 졸작

---
+++ main.py
@@ -0,0 +1,47 @@
+def embed_file(file):
+    # 업로드한 파일을 캐시 디렉토리에 저장합니다.
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+    # 단계 1: 문서 로드(Load Documents)
+    loader = PyMuPDFLoader(file_path)
+    docs = loader.load()
+
+    # 단계 2: 문서 분할(Split Documents)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+    split_documents = text_splitter.split_documents(docs)
+
+    # 단계 3: 임베딩(Embedding) 생성
+    embeddings = OpenAIEmbeddings()
+
+    # 단계 4: DB 생성(Create DB) 및 저장
+    # 벡터스토어를 생성합니다.
+    vectorstore = FAISS.from_documents(documents=split_documents, embedding=embeddings)
+
+    # 단계 5: 검색기(Retriever) 생성
+    # 문서에 포함되어 있는 정보를 검색하고 생성합니다.
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+# 체인 생성
+def create_chain(retriever, model_name="gpt-4o"):
+    # prompt = load_prompt(prompt_filepath, encoding="utf-8")
+    # 단계 6: 프롬프트 생성(Create Prompt)
+    # 프롬프트를 생성합니다.
+    prompt = load_prompt("prompts/pdf-rag.yaml", encoding="utf-8")
+
+    # 단계 7: 언어모델(LLM) 생성
+    # 모델(LLM) 을 생성합니다.
+    llm = ChatOpenAI(model_name=model_name, temperature=0)
+
+    # 단계 8: 체인(Chain) 생성
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    return chain
